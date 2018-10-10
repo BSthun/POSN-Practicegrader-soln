f0076ddddc7fee089e8d9bddb99d41e553b5331c
Bhumjate <bsthun@bbese.com>
2018-10-10 23:43:26 +0700

    Create ChooseBox.cpp

---
+++ BasicIfElse/ChooseBox.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+    int siz[3];
+    cin>>siz[2]>>siz[1]>>siz[0];
+    int box[3][3] = {
+        8,10,15,
+        12,15,25,
+        20,40,50
+    };
+
+    bool co=false;
+    for(int i=0; i<3; i++){
+        bool cor=true;
+        for(int c=0; c<3;c++){
+            if(siz[c] > box[i][c]) { cor=false; break; }
+        }
+        if(!cor)  continue;
+        co=true;
+        cout<<i+1<<endl;
+        cout<<(box[i][0]*box[i][1]*box[i][2])-(siz[0]*siz[1]*siz[2]);
+        break;
+    }
+    if(!co){
+        cout<<"Oversize product"<<endl;
+        cout<<(siz[0]*siz[1]*siz[2])-(box[2][0]*box[2][1]*box[2][2]);
+    }
+}
